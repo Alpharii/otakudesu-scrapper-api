1f521e270724f5539993385c0191079abb24aad3
alpharii <alphari404@gmail.com>
2026-02-06 18:28:54 +0700

    feat: remove listen & adjust package json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import Elysia from "elysia";
 import { api } from "./app";
 
-const app = new Elysia();
-
-app.get("/", () => 
+export default new Elysia()
+  .get("/", () => 
   "Rutenya ada di /api\n\nDokumentasi: https://github.com/Alpharii/otakudesu-scrapper-api"
-);
-
-app.use(api);
-app.listen(3000);
-
-console.log(`Server berjalan di http://localhost:3000`);
+  )
+  .use(api)
